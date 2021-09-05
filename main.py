b5c4349d5ee82307391d9cb969e4ed752c2fd50b
Pavel Dubov <PADking888@yandex.ru>
2021-09-05 18:05:26 +0300

    feat: Render six wine cards obtained from .xlsx

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from typing import Dict, List
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+import pandas
 
 
 env = Environment(
@@ -21,9 +23,23 @@ def get_years_of_life(year_of_origin, avg_days_in_year):
     return years_of_life
 
 
+def get_wines(filename) -> List[Dict]:
+    new_columns_names = ['name', 'sort', 'price', 'image']
+    df_table_about_wines = pandas.read_excel(filename)
+
+    df_table_about_wines.columns = new_columns_names
+    df_table_about_wines.price = df_table_about_wines.price.astype(int)
+
+    wines = df_table_about_wines.to_dict(orient='records')
+
+    return wines
+
+
 rendered_page = template.render(
     years_of_life=str(get_years_of_life(1920, 365.25)),
+    wines=get_wines('for_project/wine.xlsx')
 )
+
 
 with open('index.html', 'w', encoding='utf8') as file:
     file.write(rendered_page)
