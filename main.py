e22ebed8ebfd9ae832db8092d5ac2bd210450abd
Pavel Dubov <PADking888@yandex.ru>
2021-09-06 16:38:52 +0300

    feat: Groups Rose's goods

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+import pprint
 from typing import Dict, List
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import pandas
-
-
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-
-template = env.get_template('template.html')
 
 
 def get_years_of_life(year_of_origin, avg_days_in_year):
@@ -35,15 +28,52 @@ def get_wines(filename) -> List[Dict]:
     return wines
 
 
-rendered_page = template.render(
-    years_of_life=str(get_years_of_life(1920, 365.25)),
-    wines=get_wines('for_project/wine.xlsx')
-)
+def get_goods_by_categories(filename) -> Dict[str, List[Dict]]:
+    # new_columns_names = ['category', 'name', 'sort', 'price', 'image']
+    df_table_about_goods = pandas.read_excel(filename, keep_default_na=False)
+
+    # df_table_about_goods.columns = new_columns_names
+    # df_table_about_goods.price = df_table_about_goods.price.astype(int)
+
+    goods_by_categories = (
+        df_table_about_goods.groupby('Категория')
+                            .apply(lambda g: g.to_dict(orient='records'))
+                            .apply(sort_)
+                            .to_dict()
+    )
+
+    return goods_by_categories
 
 
-with open('index.html', 'w', encoding='utf8') as file:
-    file.write(rendered_page)
+def sort_(goods_row_by_category: List[Dict]) -> List[Dict]:
+    for good in goods_row_by_category:
+        for key_of_good in sorted(good):
+            good[key_of_good] = good.pop(key_of_good)
+
+    return goods_row_by_category
 
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+def main():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    template = env.get_template('template.html')
+
+    pprint.pprint(get_goods_by_categories('for_project/wine2.xlsx'))
+
+    rendered_page = template.render(
+        years_of_life=str(get_years_of_life(1920, 365.25)),
+        wines=get_wines('for_project/wine.xlsx')
+    )
+
+    with open('index.html', 'w', encoding='utf8') as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
