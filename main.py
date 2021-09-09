720c093704126816aa260100d9bf32e8498ada19
Pavel Dubov <PADking888@yandex.ru>
2021-09-09 17:14:48 +0300

    fix: Dries code: delete redundant functions, `exclusive_category` variable

--- main.py
+++ main.py
@@ -10,30 +10,6 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 import pandas
 
 
-def get_years_of_life(year_of_origin, avg_days_in_year):
-    now = datetime.datetime.utcnow()
-    born = datetime.datetime(year_of_origin, 1, 1)
-    delta = now - born
-
-    years_of_life = int(delta.days / avg_days_in_year)
-    return years_of_life
-
-
-def get_wines(filename, columns_names_wines_mapper) -> List[Dict]:
-
-    df_table_about_wines = pandas.read_excel(filename, keep_default_na=False)
-    df_table_about_wines.rename(columns_names_wines_mapper, axis='columns',
-                                inplace=True, errors='raise')
-    df_table_about_wines.price = (
-        df_table_about_wines.price
-        .astype(int, errors='ignore')
-    )
-
-    wines = df_table_about_wines.to_dict(orient='records')
-
-    return wines
-
-
 def get_goods_by_categories(filename,
                             columns_names_goods_mapper) -> Dict[str, List[Dict]]:
 
@@ -49,19 +25,10 @@ def get_goods_by_categories(filename,
     goods_by_categories = (
         df_table_about_goods.groupby('category')
                             .apply(lambda goods: goods.to_dict(orient='records'))
-                            .apply(sort_)
                             .to_dict(into=initialized_dd)
     )
 
     return goods_by_categories
-
-
-def sort_(goods_row_by_category: List[Dict]) -> List[Dict]:
-    for good in goods_row_by_category:
-        for key_of_good in sorted(good):
-            good[key_of_good] = good.pop(key_of_good)
-
-    return goods_row_by_category
 
 
 def main():
@@ -70,29 +37,24 @@ def main():
         autoescape=select_autoescape(['html', 'xml'])
     )
 
-    columns_names_wines_mapper = {
+    columns_names_goods_mapper = {
+        'Категория': 'category',
         'Название': 'name',
         'Сорт': 'sort',
         'Цена': 'price',
-        'Картинка': 'image'
-    }
-
-    columns_names_goods_mapper = {
-        'Категория': 'category',
-        **columns_names_wines_mapper,
+        'Картинка': 'image',
         'Акция': 'best_offer'
     }
 
     config = Settings()
 
     template = env.get_template(config.HTML_TEMPLATE_NAME)
-    exclusive_category = 'Напитки'
+    age = datetime.date.today().year - 1920
 
     rendered_page = template.render(
-        years_of_life=str(get_years_of_life(1920, 365.25)),
+        years_of_life=str(age),
         goods_by_category=get_goods_by_categories(config.GOODS_FILEPATH,
                                                   columns_names_goods_mapper),
-        exclusive_category=exclusive_category
     )
 
     with open('index.html', 'w', encoding='utf8') as file:
