79d771d5835367a427a8da94defdec92c213ce8d
Pavel Dubov <PADking888@yandex.ru>
2021-09-21 14:33:25 +0300

    feat: Dries code: refactor `get_goods_by_categories` due to the properties of the `defaultdict` object

--- main.py
+++ main.py
@@ -10,23 +10,13 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 import pandas
 
 
-def get_goods_by_categories(filename,
-                            columns_names_goods_mapper) -> Dict[str, List[Dict]]:
+def get_goods_by_categories(filename) -> Dict[str, List[Dict]]:
 
     df_table_about_goods = pandas.read_excel(filename, keep_default_na=False)
-    df_table_about_goods.rename(columns_names_goods_mapper, axis='columns',
-                                inplace=True, errors='raise')
-    df_table_about_goods.price = (
-        df_table_about_goods.price
-        .astype(int, errors='ignore')
-    )
-
-    initialized_dd = defaultdict(list)
-    goods_by_categories = (
-        df_table_about_goods.groupby('category')
-                            .apply(lambda goods: goods.to_dict(orient='records'))
-                            .to_dict(into=initialized_dd)
-    )
+    goods_by_categories = defaultdict(list)
+    goods = df_table_about_goods.to_dict('records')
+    for good in goods:
+        goods_by_categories[good['Категория']].append(good)
 
     return goods_by_categories
 
@@ -37,15 +27,6 @@ def main():
         autoescape=select_autoescape(['html', 'xml'])
     )
 
-    columns_names_goods_mapper = {
-        'Категория': 'category',
-        'Название': 'name',
-        'Сорт': 'sort',
-        'Цена': 'price',
-        'Картинка': 'image',
-        'Акция': 'best_offer'
-    }
-
     config = Settings()
 
     template = env.get_template(config.HTML_TEMPLATE_NAME)
@@ -53,11 +34,10 @@ def main():
 
     rendered_page = template.render(
         years_of_life=str(age),
-        goods_by_category=get_goods_by_categories(config.GOODS_FILEPATH,
-                                                  columns_names_goods_mapper),
+        goods_by_category=get_goods_by_categories(config.GOODS_FILEPATH)
     )
 
-    with open(config.HTML_SITE_PAGE, 'w', encoding='utf8') as file:
+    with open(config.HTML_SITE_PAGE, 'w', encoding='utf-8') as file:
         file.write(rendered_page)
 
     server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
